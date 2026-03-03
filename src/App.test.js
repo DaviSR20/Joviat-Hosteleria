6f51a8f0bd3e362dbff2ea935ed52e2bdde7590f
DaviSR20 <da06ramos@gmail.com>
2026-03-03 09:26:06 +0100

    Work

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Joviat logo in top bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logoElement = screen.getByAltText(/logo joviat/i);
+  expect(logoElement).toBeInTheDocument();
 });
